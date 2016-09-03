3a14f4b58045e2e084a06776cd2eb192bd38569d
tsk takezawa <tsk.take815@gmail.com>
2016-09-03 15:54:33 +0900

    Create LogTapZoomer app

---
+++ LongTapZoomer/LongTapZoomer/presentation/viewController/ViewController.swift
@@ -0,0 +1,12 @@
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}

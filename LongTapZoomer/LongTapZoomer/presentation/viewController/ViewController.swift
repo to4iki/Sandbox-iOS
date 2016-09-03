a8f6589933872b8c8bfebaece22efc26d02af39c
tsk takezawa <tsk.take815@gmail.com>
2016-09-03 17:10:21 +0900

    LongTapZoomer: Add CellLongPressedGestureRecognizer

--- LongTapZoomer/LongTapZoomer/presentation/viewController/ViewController.swift
+++ LongTapZoomer/LongTapZoomer/presentation/viewController/ViewController.swift
@@ -6,9 +6,17 @@ final class ViewController: UIViewController {
 
     private let collectionViewDataSource = CollectionViewDataSource()
 
+    lazy var setupRecognizer: (() -> Void)? = {
+        let longPressRecognizer = UILongPressGestureRecognizer(target: self, action: .cellLongPressed)
+        longPressRecognizer.allowableMovement = 100
+        self.collectionView.addGestureRecognizer(longPressRecognizer)
+        self.setupRecognizer = nil
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        setupRecognizer?()
         collectionView.dataSource = collectionViewDataSource
         collectionView.delegate = self
         collectionView.registerNib(CollectionViewCell.nib(), forCellWithReuseIdentifier: CollectionViewCell.CellIdentifier)
@@ -20,6 +28,34 @@ final class ViewController: UIViewController {
 extension ViewController: UICollectionViewDelegate {
 
     func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
-        print("index: \(indexPath.row)")
+        print("taped index: \(indexPath.row)")
     }
+}
+
+// MARK: - Recognizer
+
+extension ViewController {
+
+    func cellLongPressed(sender: UILongPressGestureRecognizer) {
+        let point = sender.locationInView(collectionView)
+        guard let indexPath = collectionView.indexPathForItemAtPoint(point) else { return }
+
+        switch sender.state {
+        case .Began:
+            print("longPressed began index: \(indexPath.row)")
+        case .Ended:
+            print("longPressed ended index: \(indexPath.row)")
+        case .Changed:
+            print("longPressed changed index: \(indexPath.row)")
+        default:
+            break
+        }
+    }
+}
+
+// MARK: - Selector
+
+private extension Selector {
+
+    static let cellLongPressed = #selector(ViewController.cellLongPressed(_:))
 }

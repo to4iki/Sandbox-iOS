5cd7ce7f255ae1b3a77879c22dbdfb7a9f91283f
tsk takezawa <tsk.take815@gmail.com>
2016-09-07 01:01:24 +0900

    FastlaneBox: Add titleLabel

--- FastlaneBox/FastlaneBox/ViewController.swift
+++ FastlaneBox/FastlaneBox/ViewController.swift
@@ -2,6 +2,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet private weak var titleLabel: UILabel!
+
     override func viewDidLoad() {
         super.viewDidLoad()
     }

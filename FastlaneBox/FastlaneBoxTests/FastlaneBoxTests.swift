473be807993a257e0c072a4535f5f83bac065716
tsk takezawa <tsk.take815@gmail.com>
2016-09-06 19:41:32 +0900

    Create FastlaneBox app

---
+++ FastlaneBox/FastlaneBoxTests/FastlaneBoxTests.swift
@@ -0,0 +1,19 @@
+import XCTest
+@testable import FastlaneBox
+
+class FastlaneBoxTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testExample() {
+        let actual = 1 + 2
+        let expected = 3
+        XCTAssert(actual == expected)
+    }
+}

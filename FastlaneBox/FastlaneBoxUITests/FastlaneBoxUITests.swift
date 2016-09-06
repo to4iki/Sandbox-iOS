d72fde628d1ac13c85de55dfa9579a6dee095e19
tsk takezawa <tsk.take815@gmail.com>
2016-09-07 01:06:24 +0900

    FastlaneBox: Add UITestBuildTarget

---
+++ FastlaneBox/FastlaneBoxUITests/FastlaneBoxUITests.swift
@@ -0,0 +1,18 @@
+import XCTest
+
+final class FastlaneBoxUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        continueAfterFailure = false
+        XCUIApplication().launch()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testExample() {
+    }
+}

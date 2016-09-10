98d768990b99dd84c02dce546ecac4f448aea885
tsk takezawa <tsk.take815@gmail.com>
2016-09-11 01:53:13 +0900

    FastlaneBox: Add snapshot point

--- FastlaneBox/FastlaneBoxUITests/FastlaneBoxUITests.swift
+++ FastlaneBox/FastlaneBoxUITests/FastlaneBoxUITests.swift
@@ -14,11 +14,28 @@ final class FastlaneBoxUITests: XCTestCase {
 
     override func tearDown() {
         super.tearDown()
+        XCUIApplication().terminate()
     }
     
-    func testTitleLabel() {
+    func testMainPage() {
         let app = XCUIApplication()
         XCTAssertTrue(app.staticTexts["titleLabel"].exists)
+        XCTAssertTrue(app.navigationBars["Main"].staticTexts["Main"].exists)
         snapshot("launch")
+    }
+
+    func testTablePage() {
+        let app = XCUIApplication()
+
+        func waitForMovedTablePage() {
+            let element = app.navigationBars["Table"].staticTexts["Table"]
+            let existsPredicate = NSPredicate(format: "exists == true")
+            expectationForPredicate(existsPredicate, evaluatedWithObject: element, handler: nil)
+            waitForExpectationsWithTimeout(10, handler: nil)
+        }
+
+        app.buttons["nextPageButton"].tap()
+        waitForMovedTablePage()
+        snapshot("tablePage")
     }
 }
